abf53729f86a98ff5683e11c007bc59b3898ee05
ckm_MacOS <k1chen@ucsd.edu>
2023-01-20 00:20:23 -0800

    0:20

--- app/src/test/java/edu/ucsd/cse110/lab2/BetterMainActivityTest.java
+++ app/src/test/java/edu/ucsd/cse110/lab2/BetterMainActivityTest.java
@@ -1,11 +1,18 @@
 package edu.ucsd.cse110.lab2;
 
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Lifecycle;
 import androidx.test.core.app.ActivityScenario;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
 
-public class BetterMainActivityTest {
+@RunWith(RobolectricTestRunner.class)
+public class BetterMainActivityTest extends AppCompatActivity {
     @Test
     public void PartThreeTest(){
         var scenario = ActivityScenario.launch(MainActivity.class);
@@ -13,7 +20,16 @@ public class BetterMainActivityTest {
         scenario.moveToState(Lifecycle.State.STARTED);
 
         scenario.onActivity(activity -> {
-            
+            var one = activity.findViewById(R.id.btn_one);
+            var plus = activity.findViewById(R.id.btn_plus);
+            var equal = activity.findViewById(R.id.btn_equals);
+            TextView result = (TextView)activity.findViewById(R.id.display);
+            one.performClick();
+            plus.performClick();
+            one.performClick();
+            equal.performClick();
+
+            assertEquals(result.getText().toString(),"2");
         });
     }
 }
--- app/src/test/java/edu/ucsd/cse110/lab2/UtilitiesTest.java
+++ app/src/test/java/edu/ucsd/cse110/lab2/UtilitiesTest.java
@@ -1,2 +1,14 @@
-package edu.ucsd.cse110.lab2;public class UtilitiesTest {
+package edu.ucsd.cse110.lab2;
+import static org.junit.Assert.*;
+
+import android.net.Uri;
+
+import org.junit.Test;
+
+public class UtilitiesTest {
+    @Test
+    public void test_trimDisplayStr(){
+        assertEquals("12.32",Utilities.trimDisplayStr("12.320"));
+        assertEquals("13",Utilities.trimDisplayStr("13.0"));
+    }
 }
